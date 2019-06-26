3e43161ca0be153bbbb5ec66d9f185fd2d4523b9
Vinicius Vaz <viniciusvaz@MacBook-Pro-de-Vinicius.local>
2019-06-26 03:39:57 -0300

    feat: Added dynamoDb connection

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,9 +1,12 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk');
 const LaunchRequestHandler = require('./handlers/LaunchRequest')
 const GetRemoteDataHandler = require('./handlers/GetRemoteData')
+const db = require('./utils/db');
+const dynamoDBTableName = "alexa-stocks";
+const GENERAL_REPROMPT = "O que deseja fazer?";
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
@@ -49,15 +52,62 @@ const ErrorHandler = {
   },
 };
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const InProgressAddMovieIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' &&
+      request.intent.name === 'AddMovieIntent' &&
+      request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    const currentIntent = handlerInput.requestEnvelope.request.intent;
+    return handlerInput.responseBuilder
+      .addDelegateDirective(currentIntent)
+      .getResponse();
+  }
+}
+
+const AddMovieIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AddMovieIntent';
+  },
+  async handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const movieName = slots.MovieName.value;
+    return db.addMovie(movieName, userID)
+      .then((data) => {
+        const speechText = `You have added movie ${movieName}. You can say add to add another one or remove to remove movie`;
+        return responseBuilder
+          .speak(speechText)
+          .reprompt(GENERAL_REPROMPT)
+          .getResponse();
+      })
+      .catch((err) => {
+        console.log("Error occured while saving movie", err);
+        const speechText = "we cannot save your movie right now. Try again!"
+        return responseBuilder
+          .speak(speechText)
+          .getResponse();
+      })
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     GetRemoteDataHandler,
     HelpIntentHandler,
+    InProgressAddMovieIntentHandler,
+    AddMovieIntentHandler,
     CancelAndStopIntentHandler
   )
   .addErrorHandlers(ErrorHandler)
+  .withTableName(dynamoDBTableName)
+  .withAutoCreateTable(true)
   .lambda();
 
